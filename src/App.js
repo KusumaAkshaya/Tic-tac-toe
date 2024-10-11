4398c0f6f3c459ba34c27c6f18a5729af00fbfeb
KusumaAkshaya <sriramaabhayam@gmail.com>
2024-10-11 13:35:43 +0530

    now the game is finished lets add time travel to it

--- src/App.js
+++ src/App.js
@@ -10,18 +10,34 @@ function Square({value, onSquareClick}){
 }
 
 export default function Board(){
-    
+    const [xIsNext, setxIsNext] = useState(true) // to vary btw x and o
     const [squares, setsquares] = useState(Array(9).fill(null))
 
     function HandleClick(i){
+      if(squares[i] || calculateWinner(squares)){  //checks whether the square is empty, if not empty it will return 
+        return;                                    //also checks if game is finished or not
+      }
       const nextSquare = squares.slice();
-      nextSquare[i] = 'X';
-      setsquares(nextSquare)
-      console.log(squares)
+      if(xIsNext) {  
+        nextSquare[i] = 'X';
+      } else {     
+        nextSquare[i] = 'O';
+      }
+      setsquares(nextSquare)  
+      setxIsNext(!xIsNext) // updates X and O 
     } 
+
+    const winner = calculateWinner(squares)
+    let status;
+    if(winner){
+      status = 'Winner is '+ winner    //if winner is not null, shows winner
+    } else {
+      status = 'Next Player is '+ (xIsNext ? 'X' : 'O') //if winner is null, shows next player
+    }
 
     return(
     <>
+    <h2>{status}</h2>
     <div className="Row">
       <Square value={squares[0]} onSquareClick={() => HandleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => HandleClick(1)} />
@@ -39,6 +55,27 @@ export default function Board(){
     </div>
     </>
     );
+}
+
+
+function calculateWinner(squares){
+  const lines = [ //winning lines
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for(let i=0; i<lines.length; i++){ 
+    const [a, b, c] = lines[i]; // making a, b, c = winnings lines
+    if(squares[a] && squares[a]==squares[b] && squares[a] == squares[c]){ // checking if square is not null and all three indices having same value
+      return squares[a]
+    }
+  }
+  return null; //if all are not same returns null
 }
 
 
